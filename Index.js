49f42006ff7dc50a520932d0ceec2116be03293f
kunal-1308 <bhapkarkunal@gmail.com>
2020-03-19 13:09:50 +0530

    added dark and light modes

--- Index.js
+++ Index.js
@@ -2,3 +2,7 @@ function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+function lightMode() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+}
