288dafa5eaddfb6086bcd75052815e8d8636af0e
Kunal Bhapkar <bhapkarkunal@gmail.com>
2025-11-15 14:15:36 +0530

    links open in new tab

--- src/components/certifications/Certification.js
+++ src/components/certifications/Certification.js
@@ -22,25 +22,41 @@ function Certification() {
 				</div>
 				<div className="col-12 col-md-9 d-flex flex-column align-items-center justify-content-center">
 					<div className="p-2 w-100 text-center text-md-left">
-						<a href="https://www.coursera.org/account/accomplishments/verify/N9DQ4KQZ2UDP">
+						<a
+							href="https://www.coursera.org/account/accomplishments/verify/N9DQ4KQZ2UDP"
+							target="_blank"
+							rel="noreferrer"
+						>
 							<h4>Python for Data Science and AI</h4>
 						</a>
 					</div>
 
 					<div className="p-2 w-100 text-center text-md-left">
-						<a href="https://www.udemy.com/certificate/UC-3a098948-df32-41a4-8eb9-363e6b9e69d5/">
+						<a
+							href="https://www.udemy.com/certificate/UC-3a098948-df32-41a4-8eb9-363e6b9e69d5/"
+							target="_blank"
+							rel="noreferrer"
+						>
 							<h4>React - The Complete Guide</h4>
 						</a>
 					</div>
 
 					<div className="p-2 w-100 text-center text-md-left">
-						<a href="https://www.coursera.org/account/accomplishments/verify/D5EYTAXKKRUA">
+						<a
+							href="https://www.coursera.org/account/accomplishments/verify/D5EYTAXKKRUA"
+							target="_blank"
+							rel="noreferrer"
+						>
 							<h4>Machine Learning with Python</h4>
 						</a>
 					</div>
 
 					<div className="p-2 w-100 text-center text-md-left">
-						<a href="https://www.coursera.org/account/accomplishments/specialization/certificate/NJXATYUYJWAU">
+						<a
+							href="https://www.coursera.org/account/accomplishments/specialization/certificate/NJXATYUYJWAU"
+							target="_blank"
+							rel="noreferrer"
+						>
 							<h4>IBM Data Science Specialization</h4>
 						</a>
 					</div>
